7cbfdc9b0168d7a519eeff7e23287ab022ece685
Alejandro Garcia <agrcrobles@gmail.com>
2018-03-24 20:31:02 +0100

    Added docker

--- api/src/index.js
+++ api/src/index.js
@@ -2,9 +2,10 @@ const buildFastify = require("./app");
 
 const fastify = buildFastify();
 if (require.main === module) {
-  fastify.listen(3000, err => {
+  fastify.listen(3000, '0.0.0.0', err => {
     if (err) console.error(err);
     console.log(`server listening on ${fastify.server.address().port}`);
+    
   });
 } else {
   // required as a module => executed on aws lambda
