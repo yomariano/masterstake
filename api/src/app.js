6af37dae73de868277f7138a33d44cfcf7ca88a9
Fernando Callejon <fcallejon@gmail.com>
2018-03-25 13:31:33 +0100

    WIP adding session

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,16 @@
 const Fastify = require("fastify");
 const nconf = require("nconf");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 
 const postSignin = require("./endpoints/postSignin");
 const postJoin = require("./endpoints/postJoin");
 const getMasternodes = require("./endpoints/getMasternodes");
 const getMyMasternodes = require("./endpoints/getMyMasternodes");
 const { usersSchema, nodesSchema } = require("./mongooseSchemas");
+const passport = require("passport");
+const auth = require("./googleOAuth");
 
 const users = mongoose.model("users", usersSchema);
 const nodes = mongoose.model("nodes", nodesSchema);
@@ -32,6 +36,44 @@ function buildFastify() {
     }
   );
 
+  fastify.use(
+    cookieSession({
+      name: "session",
+      keys: ["123"]
+    })
+  );
+  fastify.use(cookieParser());
+
+  auth(passport, nconf.get("oauth:google"));
+  fastify.use(passport.initialize());
+
+  fastify.get(
+    "/auth/g/back",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+      req.raw.session.token = req.raw.user.token;
+      res.redirect("/");
+    }
+  );
+
+  fastify.get("/", (req, res) => {
+    const { session } = req.raw;
+    console.log(res.raw.IncomingMessage.cookies);
+    res.send();
+    return;
+    if (session.token) {
+      res.cookie("token", session.token);
+      res.json({
+        status: "session cookie set"
+      });
+    } else {
+      res.cookie("token", "");
+      res.json({
+        status: "session cookie not set"
+      });
+    }
+  });
+
   fastify.addContentTypeParser(
     "application/json",
     { parseAs: "string" },
@@ -45,6 +87,12 @@ function buildFastify() {
       }
     }
   );
+
+  fastify.get("/logout", (req, res) => {
+    req.logout();
+    req.session = null;
+    res.redirect("/");
+  });
 
   fastify.route(getMasternodes);
   fastify.route(getMyMasternodes);
